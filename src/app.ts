d588e0ce71ecc712f786b9fc0f62a6e6039ce702
Joshua Muster <joshuamuster@gmail.com>
2023-09-03 00:47:12 -0700

    remove unused files from carious tutorials

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ import {Router, RouterConfiguration} from 'aurelia-router';
       config.options.root = '/';
       config.map([
         { route: '',              moduleId: PLATFORM.moduleName('no-selection'),   title: 'Select'},
-        { route: 'contacts/:id',  moduleId: PLATFORM.moduleName('contact-detail'), name:'contacts' }
+        // { route: 'contacts/:id',  moduleId: PLATFORM.moduleName('contact-detail'), name:'contacts' }
       ]);
   
       this.router = router;
